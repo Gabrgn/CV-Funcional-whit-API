1f5e045de5b6fc2210bb9b3fe5dbde1acea2d49e
Gabriel <gabrielbrgn4701@gmail.com>
2023-02-10 00:06:27 -0300

    Este es un cv funcional que consume datos de una api

---
+++ ajax.js
@@ -0,0 +1,63 @@
+//console.log("correcto");
+
+document.querySelector("#botton").addEventListener("click", traerDatos());
+
+function traerDatos() {
+  //console.log("dentro de la function");
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "https://randomuser.me/api/", true);
+
+  xhttp.send();
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      //console.log(this.responseText);
+
+      let datos = JSON.parse(this.responseText);
+      console.log(datos.results);
+
+      let title = document.querySelector("#title");
+      title.innerHTML = "";
+      let contenido = document.querySelector("#contenido");
+      contenido.innerHTML = "";
+      let propiedadesDos = document.querySelector("#propiedadesDos");
+      contenido.innerHTML = "";
+      let img = document.querySelector("#img");
+      contenido.innerHTML = "";
+
+      //const obj = { info: "info", results: "results" };
+      for (let item of datos.results) {
+        //console.log(item.email);
+        title.innerHTML += `
+        <h2>${item.name.first} ${item.name.last} </h2>
+        `;
+        contenido.innerHTML += `
+        <div> Edad: ${item.dob.age} <br /> Fecha de Nacimiento: ${item.dob.date} <br /> Genero: ${item.gender}  </div>
+        `;
+        propiedadesDos.innerHTML += `
+        <p> Cel: ${item.cell} <br /> Email: ${item.email} <br/> Direccion: ${item.location.street.name} ${item.location.street.number} - ${item.location.city}, ${item.location.country} </p>
+        `;
+        img.innerHTML += `
+        <img src="${item.picture.large}" alt="Foto de Perfil" id="img"> </img>
+        `;
+      }
+    }
+  };
+}
+
+function mostrar() {
+  document.getElementById("aptitudes").style.display = "block";
+}
+
+function ocultar() {
+  document.getElementById("aptitudes").style.display = "none";
+}
+
+function mostrarhabilidades() {
+  document.getElementById("habilidades").style.display = "block";
+}
+
+function ocultarhabilidades() {
+  document.getElementById("habilidades").style.display = "none";
+}
